7ab4afbf4ff94e0b2d27a83435cef44ea2c2c4bf
Foxkun69 <nigg8317@gmail.com>
2024-10-03 14:01:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Đang Nghe Foxkun hát", "🎮 Đang Chơi Sextoy "];
+const statusMessages = ["🎧 Đang Nghe Foxkun hát", "🎮Đang Chơi GTA VI", "🛒Fox Store", "✨By Foxkun"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
